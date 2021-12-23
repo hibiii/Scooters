77adff0f78fc17d8c4d661af900c456f28c81770
Hibi <69405847+Hibiii@users.noreply.github.com>
2021-12-23 19:47:38 -0300

    Basic branding

--- src/main/java/hibi/scooters/ClientInit.java
+++ src/main/java/hibi/scooters/ClientInit.java
@@ -1,17 +1,17 @@
-package net.fabricmc.example;
+package hibi.scooters;
 
-import net.fabricmc.api.ModInitializer;
+import net.fabricmc.api.ClientModInitializer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class ExampleMod implements ModInitializer {
+public class ClientInit implements ClientModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LogManager.getLogger("modid");
+	public static final Logger LOGGER = LogManager.getLogger("scooters");
 
 	@Override
-	public void onInitialize() {
+	public void onInitializeClient() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
--- src/main/java/hibi/scooters/mixin/ExampleMixin.java
+++ src/main/java/hibi/scooters/mixin/ExampleMixin.java
@@ -1,16 +1,17 @@
-package net.fabricmc.example.mixin;
+package hibi.scooters.mixin;
 
-import net.fabricmc.example.ExampleMod;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import hibi.scooters.ClientInit;
+
 @Mixin(TitleScreen.class)
 public class ExampleMixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
-		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
+		ClientInit.LOGGER.info("This line is printed by an example mod mixin!");
 	}
 }
