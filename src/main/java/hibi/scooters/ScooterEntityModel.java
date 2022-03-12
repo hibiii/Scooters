6065e68bd186f9f84663e35dd15f32c1b2ea70ac
Hibi <69405847+Hibiii@users.noreply.github.com>
2022-03-12 20:13:08 -0300

    Prevent clipping with the scooter and player arms

--- src/main/java/hibi/scooters/ScooterEntityModel.java
+++ src/main/java/hibi/scooters/ScooterEntityModel.java
@@ -21,29 +21,38 @@ public class ScooterEntityModel extends EntityModel<ScooterEntity> {
 	}
 
 	public static TexturedModelData model() {
-		ModelData meshdefinition = new ModelData();
-		ModelPartData partdefinition = meshdefinition.getRoot();
+		ModelData modelData = new ModelData();
+		ModelPartData modelPartData = modelData.getRoot();
 
-		partdefinition.addChild("Back", ModelPartBuilder.create().uv(0, 0).cuboid(-2.0F, -1.9F, -7.0F, 4.0F, 1.0F, 14.0F, Dilation.NONE)
-		.uv(0, 17).cuboid(-0.5F, -3.0F, 8.0F, 1.0F, 3.0F, 3.0F, Dilation.NONE)
-		.uv(4, 0).cuboid(-1.0F, -4.0F, 7.0F, 2.0F, 3.0F, 3.0F, Dilation.NONE), ModelTransform.pivot(0.0F, 24.0F, 0.0F));
+		modelPartData.addChild("Back", ModelPartBuilder.create()
+			.uv(0, 0).cuboid(-2.0F, -1.9F, -7.0F, 4.0F, 1.0F, 14.0F, Dilation.NONE) // Platform
+			.uv(0, 17).cuboid(-0.5F, -3.0F, 8.0F, 1.0F, 3.0F, 3.0F, Dilation.NONE)  // Back tire
+			.uv(4, 0).cuboid(-1.0F, -4.0F, 7.0F, 2.0F, 3.0F, 3.0F, Dilation.NONE),  // Tire cover
+		ModelTransform.pivot(0.0F, 24.0F, 0.0F));
 
-		ModelPartData Steering = partdefinition.addChild("Steering", ModelPartBuilder.create().uv(0, 15).cuboid(-6.0F, -17.0F, 0.0F, 12.0F, 1.0F, 1.0F, Dilation.NONE)
-		.uv(8, 17).cuboid(-0.5F, -3.0F, -4.0F, 1.0F, 3.0F, 3.0F, Dilation.NONE)
-		.uv(4, 6).cuboid(-1.0F, -4.0F, -3.0F, 2.0F, 3.0F, 3.0F, Dilation.NONE), ModelTransform.pivot(0.0F, 24.0F, -7.0F));
+		ModelPartData Steering = modelPartData.addChild("Steering", ModelPartBuilder.create()
+			.uv(0, 15).cuboid(-6.0F, -16.75F, 1.25F, 12.0F, 1.0F, 1.0F, Dilation.NONE) // Handle bar
+			.uv(8, 17).cuboid(-0.5F, -3.0F, -4.0F, 1.0F, 3.0F, 3.0F, Dilation.NONE)    // Front tire
+			.uv(4, 6).cuboid(-1.0F, -4.0F, -3.0F, 2.0F, 3.0F, 3.0F, Dilation.NONE),    // Tire cover
+		ModelTransform.pivot(0.0F, 24.0F, -7.0F));
 
-		Steering.addChild("Bar_r1", ModelPartBuilder.create().uv(0, 0).cuboid(-0.5F, -7.0F, 2.25F, 1.0F, 13.0F, 1.0F, Dilation.NONE), ModelTransform.of(0.0F, -10.0F, -3.5F, -0.1745F, 0.0F, 0.0F));
+		Steering.addChild("Bar", ModelPartBuilder.create()
+			.uv(0, 0).cuboid(-0.5F, -12.5F, -0.5F, 1.0F, 13.0F, 1.0F, Dilation.NONE), // Steering Column
+		ModelTransform.of(0.0F, -4.0F, -1.5F, -0.2443F, 0.0F, 0.0F));
 
-		Steering.addChild("Brake_r1", ModelPartBuilder.create().uv(13, 17).cuboid(0.0F, -0.5F, -0.5F, 5.0F, 1.0F, 1.0F, Dilation.NONE), ModelTransform.of(1.0F, -16.0F, 0.0F, 0.0F, 0.1745F, 0.0873F));
+		Steering.addChild("Brake", ModelPartBuilder.create()
+			.uv(13, 17).cuboid(0.0F, -0.5F, -0.5F, 5.0F, 1.0F, 1.0F, Dilation.NONE), // Brake lever
+		ModelTransform.of(1.0F, -15.75F, 1.25F, 0.0F, 0.2182F, 0.1309F));
 
-		Steering.addChild("Throttle_r1", ModelPartBuilder.create().uv(26,15).cuboid(-0.5F, -1.0F, -0.5F, 1.0F, 2.0F, 1.0F, Dilation.NONE), ModelTransform.of(-1.5F, -16.5F, 0.75F, 0.3491F, 0.0F, 0.0F));
+		Steering.addChild("Throttle", ModelPartBuilder.create()
+			.uv(26,15).cuboid(-0.5F, -1.0F, -0.5F, 1.0F, 2.0F, 1.0F, Dilation.NONE), // Throttle lever
+		ModelTransform.of(-1.5F, -16.25F, 2.0F, 0.3927F, 0.0F, 0.0F));
 
-		return TexturedModelData.of(meshdefinition, 64, 64);
+		return TexturedModelData.of(modelData, 64, 64);
 	}
 
 	@Override
 	public void setAngles(ScooterEntity entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
-
 	}
 
 	@Override
