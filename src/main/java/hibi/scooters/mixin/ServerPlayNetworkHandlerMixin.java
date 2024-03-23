05d3144a204669a27aba1beeca4111ab3e57bad9
hibi <69405847+hibiii@users.noreply.github.com>
2024-03-23 13:41:29 -0300

    Use ModifyExpressionValue to prevent collisions

--- src/main/java/hibi/scooters/mixin/ServerPlayNetworkHandlerMixin.java
+++ src/main/java/hibi/scooters/mixin/ServerPlayNetworkHandlerMixin.java
@@ -1,17 +1,21 @@
 package hibi.scooters.mixin;
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.Constant;
-import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.spongepowered.asm.mixin.injection.At;
+
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 
 import hibi.scooters.ScooterEntity;
 import net.minecraft.server.network.ServerPlayNetworkHandler;
 
 @Mixin(ServerPlayNetworkHandler.class)
 public class ServerPlayNetworkHandlerMixin {
-    @ModifyConstant(
+    @ModifyExpressionValue(
         method = "onVehicleMove",
-        constant = @Constant(doubleValue = 0.0625D)
+        at = @At(
+            value = "CONSTANT",
+            args = "doubleValue=0.0625"
+        )
     )
     double accountPathsForScooters(double p) {
         if (((ServerPlayNetworkHandler)(Object)this).player.getVehicle() instanceof ScooterEntity) {
